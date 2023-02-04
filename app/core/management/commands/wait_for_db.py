857d978fcbdb9089ca3c67b7dddf27d148addd98
Clint Burgess <cburgess@achdidaho.org>
2023-02-04 08:16:54 -0700

    Updated code.

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -4,7 +4,7 @@ Django command to wait for the database to be available.
 from django.core.management.base import BaseCommand
 
 
-class Commmand(BaseCommand):
+class Command(BaseCommand):
     """Django command to wait for database."""
 
     def handle(self, *args, **options):
